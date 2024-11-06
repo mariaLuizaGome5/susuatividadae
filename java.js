9dc51fc77cf973a28449120108ba68b7329604ae
mariaLuizaGome5 <22302506@aluno.cotemig.com.br>
2024-11-06 14:29:04 -0300

    Add files via upload

---
+++ java.js
@@ -0,0 +1,14 @@
+addEventListener("DOMContentLoaded",()=>{var recebtel=localStorage.getItem("telefone")
+document.getElementById("telefone").innerHTML=recebtel})
+
+addEventListener("DOMContentLoaded",()=>{var recebtel=localStorage.getItem("cpf")
+document.getElementById("cpf").innerHTML=recebtel})
+
+addEventListener("DOMContentLoaded",()=>{var recebtel=localStorage.getItem("codigo")
+document.getElementById("codigo").innerHTML=recebtel})
+
+addEventListener("DOMContentLoaded",()=>{var recebtel=localStorage.getItem("cnpj")
+document.getElementById("cnpj").innerHTML=recebtel})
+
+addEventListener("DOMContentLoaded",()=>{var recebtel=localStorage.getItem("cep")
+document.getElementById("cep").innerHTML=recebtel})
